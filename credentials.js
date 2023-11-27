f01d6243e7ecbb8a8f82bcdf39afad2cf92e1ab5
KirendarshiniR <143775511+KirendarshiniR@users.noreply.github.com>
2023-11-27 23:36:17 +0530

    Create credentials.js

---
+++ credentials.js
@@ -0,0 +1,9 @@
+
+/*
+ How this obtained ? ->see the readme.md file 
+*/
+const CLIENT_ID = "Enter Your Client ID";
+const CLEINT_SECRET = "Enter Your Cient Secret";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN ="Enter the exchanged Refresh Token";
+module.exports = { CLIENT_ID, CLEINT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
